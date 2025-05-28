47b05ddd01fe6c5cdf8039a1eb36213133a948bc
a.harlamov <a.harlamov@profiscope.io>
2025-05-28 23:51:36 +0300

    Update code due to new style

--- service.py
+++ service.py
@@ -1,29 +1,31 @@
 import bentoml
 import torch
-from bentoml.io import JSON, Text
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 
 
 tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
 model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
 
-svc = bentoml.Service("sentiment_classifier", runners=[])
 
+@bentoml.service
+class SentimentClassifier:
+    tokenizer = tokenizer
+    model = model
 
-@svc.api(input=Text(), output=JSON())
-def classify(text: str) -> dict:
-    inputs = tokenizer(text, return_tensors="pt")
-    with torch.no_grad():
-        logits = model(**inputs).logits
-    probabilities = torch.softmax(logits, dim=1).tolist()[0]
-    predicted_class = int(torch.argmax(logits, dim=1).item())
+    @bentoml.api
+    def classify(self, text: str) -> dict:
+        inputs = self.tokenizer(text, return_tensors="pt")
+        with torch.no_grad():
+            logits = self.model(**inputs).logits
+        probabilities = torch.softmax(logits, dim=1).tolist()[0]
+        predicted_class = int(torch.argmax(logits, dim=1).item())
 
-    sentiment = "positive" if predicted_class == 1 else "negative"
-    return {
-        "text": text,
-        "sentiment": sentiment,
-        "confidence": {
-            "negative": round(probabilities[0], 2),
-            "positive": round(probabilities[1], 2)
+        sentiment = "positive" if predicted_class == 1 else "negative"
+        return {
+            "text": text,
+            "sentiment": sentiment,
+            "confidence": {
+                "negative": round(probabilities[0], 2),
+                "positive": round(probabilities[1], 2),
+            },
         }
-    }
